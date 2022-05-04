a5fda21c5c250a62582d3260b7c68e17144e824c
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-05-04 12:30:08 -0300

    Sign-in screen created

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useState } from "react"
 
 import SignIn from "./SignIn"
 import SignUp from "./SignUp"
@@ -6,17 +7,23 @@ import MainPage from "./MainPage"
 import Entry from "./Entry"
 import Payment from "./Payment"
 
+import UserContext from "../contexts/UserContext"
+
 export default function App() {
+  const [user, setUser] = useState({})
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/entry" element={<Entry />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="*" element={<p>This page does not exist!</p>} />
-      </Routes>
-    </BrowserRouter>
+    <UserContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/entry" element={<Entry />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="*" element={<p>This page does not exist!</p>} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   )
 }
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,12 +1,105 @@
+import { useContext, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios"
 import styled from "styled-components"
 
+import UserContext from "../contexts/UserContext"
+
 export default function SignIn() {
+  const navigate = useNavigate()
+  const [valid, setValid] = useState(true)
+  const { setUser } = useContext(UserContext)
+
+  function submitForm(e) {
+    e.preventDefault()
+    const URI = "http://localhost:5000/signin"
+
+    const promisse = axios.post(URI, {
+      email: e.target[0].value,
+      password: e.target[1].value,
+    })
+    promisse.then((response) => {
+      setUser(response.data)
+      navigate("/main")
+    })
+    promisse.catch((e) => {
+      setValid(false)
+    })
+  }
+
   return (
     <Main>
-      <h1>Sign In</h1>
+      <h1>MyWallet</h1>
+      <form onSubmit={(e) => submitForm(e)}>
+        <input type="email" placeholder="E-mail" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit">Enter</button>
+      </form>
+      <StyledLink to={"/sign-up"}>First time? Sign-up!</StyledLink>
+      {valid ? <></> : <p>Failed to Sign-in...</p>}
     </Main>
   )
 }
 
 // STYLED COMPONENTS
-const Main = styled.main``
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 90%;
+  margin: 170px auto 0;
+
+  form {
+    width: 100%;
+  }
+
+  h1 {
+    font-family: "Saira Stencil One", cursive;
+    margin-bottom: 24px;
+
+    font-size: 32px;
+    color: #ffffff;
+  }
+
+  input {
+    width: 100%;
+    height: 58px;
+
+    margin-bottom: 13px;
+    border-radius: 5px;
+    border: none;
+    outline: none;
+
+    font-size: 20px;
+    padding: 0 15px;
+  }
+
+  button {
+    width: 100%;
+    height: 46px;
+
+    margin-bottom: 36px;
+    border: none;
+    border-radius: 5px;
+    background-color: #a328d6;
+
+    font-size: 20px;
+    color: #ffffff;
+    font-weight: 700;
+    cursor: pointer;
+  }
+
+  p {
+    margin-top: 24px;
+    font-size: 20px;
+    font-weight: 700;
+    color: var(--error);
+  }
+`
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: 15px;
+  font-weight: 700;
+  color: #ffffff;
+`
